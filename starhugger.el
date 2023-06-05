cb5405ac13657b9215b6eaa9a0c042aafb83351e
Daanturo <daanturo@gmail.com>
2023-06-05 23:20:08 +0700

    Version 0.3.0
    Update docs

--- starhugger.el
+++ starhugger.el
@@ -1,6 +1,6 @@
-;;; starhugger.el --- Unofficial Hugging Face/AI-powered text & code completion client  -*- lexical-binding: t; -*-
+;;; starhugger.el --- Hugging Face/AI-powered text & code completion client (unoffical)  -*- lexical-binding: t; -*-
 
-;; Version: 0.3.0-git
+;; Version: 0.3.0
 ;; Package-Requires: ((emacs "28.2") (compat "29.1.4.0") (dash "2.18.0") (s "1.13.1") (spinner "1.7.4"))
 ;; Keywords: completion, convenience, languages
 
